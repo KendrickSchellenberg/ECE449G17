0e3dcd2afed43b8c6cdba5542565f723d3fe3bf7
Raymart Bless C. Datuin <raymartd1@gmail.com>
2023-11-25 22:50:47 -0700

    Made basic template pydoc, very vague as of the moment. Will become more detailed as implementation begins

--- fuzzyGAController.py
+++ fuzzyGAController.py
@@ -2,22 +2,86 @@ from G17_controller import G17Controller
 
 class FuzzyGAController(G17Controller):
     """
-    
+    Child of G17Controller. This is only supposed to implement Fuzzy Logic - Genetic Algorithm
+    To improve the performance of the AI model created to beat the Asteroid game
+
+    ...
+    Methods
+    ```````
+    make_fuzzy_controller:
+        creates a fuzzy controller with generated chromosome
+    fitness:
+        Evalautes the fuzzy controller with given chromosome on a test scenario.
+        Evaluated based on a criteria.
+    gen_chromosome:
+        From Kendrick's code, literally just random...
+    run:
+        Run the genetic algorithm
+    test:
+        Test the genetic algorithm on the drivers.py
     """
+    
     def make_fuzzy_controller(self, chromosome):
+        """
+
+        Parameters
+        ``````````
+        chromosome (Gene) : 
+
+        
+        Returns
+        ```````
+        """
         pass
 
     def fitness(self, chromosome):
+        """
+        
+        Parameters
+        ``````````
+        chromosome (Gene) : 
+
+        Returns
+        ```````
+        """
         pass
 
     # We are going to test on scenario
     def execute_fuzzy_inference(self, thrust_sim, fire_sim):
+        """
+        
+        Parameters
+        ``````````
+        chromosome (Gene) : 
+
+        Returns
+        ```````
+        """
         pass
 
     def gen_chromosome(self):
+        """
+        
+        Parameters
+        ``````````
+        chromosome (Gene) : 
+
+        Returns
+        ```````
+        """
+        
         pass
 
     def run(self):
+        """
+        
+        Parameters
+        ``````````
+        chromosome (Gene) : 
+
+        Returns
+        ```````
+        """
         ga = EasyGA.GA()
         # ga.gene_impl = self 
         ga.chromosome_impl = self.gen_chromosome
@@ -35,3 +99,4 @@ class FuzzyGAController(G17Controller):
         ga.evolve()
         ga.print_best_chromosome()
         # Now what do i do with the best chromosome?
+        #### Reference Kendrick's Lab on Test Dataset Evaluation
